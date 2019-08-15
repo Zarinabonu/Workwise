bd5ae50450df3687d3b4c56d0fd43fe3d95caa86
Zarina <zarinabonu199924@gmail.com>
2019-08-15 18:14:12 +0500

    l

--- Job/views.py
+++ Job/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from Job.models import Post
+
+
+class Category_ListView(ListView):
+    template_name = 'index.html'
+    model = Post
+    context_object_name = 'job_posts'
 
 # Create your views here.
--- user/views.py
+++ user/views.py
@@ -1,19 +1,30 @@
+from itertools import count
+
 from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.models import User
+from django.db.models import Count, Q
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
+from django.views.generic import ListView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+import Job
+from Job.models import Post, Category, Time
+from user.models import Profile
 
 
 class User_LogInView(View):
-    def get(self,request):
-        return render(request,'login.html')
+    def get(self, request):
+        return render(request, 'register.html')
 
-    def post(self,request):
+    def post(self, request):
         username = request.POST['username']
         password = request.POST['password']
 
-        user = authenticate(request, username=username,password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             # Redirect to a success page.
@@ -25,17 +36,36 @@ class User_LogInView(View):
         return redirect(reverse('test'))
 
 
-
-
 class User_LogOut(View):
     def get(self, request):
         logout(request)
         return redirect(reverse('test'))
 
 
+friend = 0
+
+
 class Call_Index(View):
     def get(self, request):
-        return render(request, 'index.html')
+
+        u = User.objects.get(username=request.user.username)
+        f_set = u.profile.following.all()
+        ids = f_set.values_list('id', flat=True)
+        suggestions = User.objects.exclude(id__in=ids).filter(~Q(id=u.id)).annotate(count_suggest=Count('following')).order_by('-count_suggest')[:5]
+        s_list = []
+        for friend_my in f_set:
+            for friend_f in friend_my.profile.following.all():
+                if friend_f not in f_set:
+                    if f_set.filter(profile__following__id=friend_f.id).count() >= 2:
+                        if friend_f not in s_list:
+
+                            s_list.append(friend_f)
+
+        print(s_list)
+        posts = Post.objects.all()
+        categories = Category.objects.all()
+        times = Time.objects.all()
+        return render(request, 'index.html', {'suggestions': suggestions, 'suggest': s_list, 'categories': categories, 'time': times})
 
 
 class User_followers(View):
@@ -47,13 +77,20 @@ class User_followers(View):
         f.save()
 
 
-class User_following(View):
-    def get(self, request):
+class User_following(APIView):
+    def post(self, request):
         u = request.user
-        print(request.user.username,' ID  ',request.user.id)
-        f = User.objects.get(id='1')
-        print('User ',f)
+        print(request.user.username, ' ID  ', request.user.id)
+        f = User.objects.get(id=request.POST.get('f_id'))
+        print('User ', f)
         u.profile.following.add(f)
 
+        # u = request.user
+        # f = User.objects.get(id='1')
+        f.profile.followers.add(u)
+
+        f.save()
+
+        return Response(status=status.HTTP_200_OK)
 
 # Create your views here.
